b8c36593dd5b40a913e2df45611d31e45a2e8e87
Sebastian Menski <sebastian.menski@googlemail.com>
2015-03-04 23:23:31 +0100

    chore(Dockerfile): use nexus api to download artifacts

--- bin/download-database-drivers.sh
+++ bin/download-database-drivers.sh
@@ -11,9 +11,9 @@ POSTGRESQL_VERSION=$(echo $POM | xmlstarlet sel -t -v //_:version.postgre)
 GITHUB="https://raw.githubusercontent.com/camunda/camunda-bpm-platform/${VERSION}"
 
 mkdir -p $MYSQL_DIR
-wget -P $MYSQL_DIR ${NEXUS}/mysql/mysql-connector-java/${MYSQL_VERSION}/mysql-connector-java-${MYSQL_VERSION}.jar
+wget -O ${MYSQL_DIR}/mysql-connector-java-${MYSQL_VERSION}.jar "${NEXUS}?r=public&g=mysql&a=mysql-connector-java&v=${MYSQL_VERSION}&p=jar"
 wget -P $MYSQL_DIR ${GITHUB}/qa/jboss7-runtime/src/main/modules/mysql/mysql-connector-java/main/module.xml
 
 mkdir -p $POSTGRESQL_DIR
-wget -P $POSTGRESQL_DIR ${NEXUS}/org/postgresql/postgresql/${POSTGRESQL_VERSION}/postgresql-${POSTGRESQL_VERSION}.jar
+wget -O ${POSTGRESQL_DIR}/postgresql-${POSTGRESQL_VERSION}.jar "${NEXUS}?r=public&g=org.postgresql&a=postgresql&v=${POSTGRESQL_VERSION}&p=jar"
 wget -P $POSTGRESQL_DIR ${GITHUB}/qa/jboss7-runtime/src/main/modules/org/postgresql/postgresql/main/module.xml
