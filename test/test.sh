d9485f30f4edc2a829fc506ad8fe740827441cd1
Sebastian Menski <sebastian.menski@googlemail.com>
2015-03-04 21:44:38 +0100

    feat(version): add tomcat 7.1.0 branch
    closes #2

--- test/test.sh
+++ test/test.sh
@@ -6,13 +6,16 @@ source ${DIR}/test_helper.sh
 
 start_container
 
-poll_log "^INFO: Server startup in" "Exception" || _exit 1 "Server not started"
+poll_log "^INFO: Server startup in" "^SEVERE" || _exit 1 "Server not started"
 
 _log "Server started"
 
 grep_log "INFO: Process Application camunda-invoice successfully deployed" || _exit 2 "Process application not deployed"
 
 _log "Process application deployed"
+
+# wait before login
+sleep 3
 
 test_login admin || _exit 3 "Unable to login to admin"
 test_login cockpit || _exit 4 "Unable to login to cockpit"
