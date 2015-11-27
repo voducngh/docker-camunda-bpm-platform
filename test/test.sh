f7eee8bd33e2983384fdab0b61aa68d624b609a5
Sebastian Menski <sebastian@menski.org>
2015-11-27 20:22:12 +0100

    chore(version): switch to 7.5.0-SNAPSHOT

--- test/test.sh
+++ test/test.sh
@@ -10,7 +10,7 @@ poll_log 'WildFly 8.2.1.Final "Tweek" started in' 'WildFly 8.2.1.Final "Tweek" s
 
 _log "Server started"
 
-grep_log 'Deployed "camunda-example-invoice-7.4.0-' || _exit 2 "Process application not deployed"
+grep_log 'Deployed "camunda-example-invoice-7.5.0-' || _exit 2 "Process application not deployed"
 
 _log "Process application deployed"
 
